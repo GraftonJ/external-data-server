06e89deab05f44c20cd3c42c5684c321f1d9e77a
Josh <grafton.josh@gmail.com>
2018-10-05 14:02:14 -0600

    Refactor Express server to send different responses

---
+++ serverExpress.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.disable('x-powered-by');
+
+app.get('/guests', function(req, res) {
+  var guests = ['Mary', 'Don'];
+  res.send(guests);
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404)
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
